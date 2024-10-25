2f0fad97c0bdc83ef6382ad8b0ed3adb1164148d
Mohammad Mehrnia <mehrnia.mohammad@yahoo.com>
2024-10-25 11:30:54 -0400

    comparing scipy to Jax CG solution

--- scripts/step0.py
+++ scripts/step0.py
@@ -1,6 +1,9 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 from jax.scipy.sparse.linalg import cg
+from scipy.sparse.linalg import cg as cgs
+import time
 
 # Example of a symmetric positive definite matrix A
 def generate_spd_matrix(size):
@@ -9,24 +12,37 @@ def generate_spd_matrix(size):
     return A
 
 # Define the size of the matrix
-n = 1000
+n = 10000
+print(f'Size: {n*n}')
+
 
 # Create a random symmetric positive definite matrix A
 A = generate_spd_matrix(n)
+A_numpy = np.array(A)
 
 # Define a random vector b
 b = jax.random.normal(jax.random.PRNGKey(1), (n,))
+b_numpy  = np.array(b)
 
 # Define the linear operator function for CG
-def A_mv(x):
-    return jnp.dot(A, x)
+# def A_mv(x):
+#     return jnp.dot(A, x)
 
-# Solve the linear system using conjugate gradient method
-x0 = jnp.zeros_like(b)  # Initial guess for x
+# Run the JAX conjugate gradient algorithm
+start = time.time()
+soln_jax, _ = cg(A, b)
+stop = time.time()
+print(f'JAX time: {stop - start}s')
 
-# Run the conjugate gradient algorithm
-solution, _ = cg(A_mv, b, x0=x0)
+# Run the Scipy conjugate gradient algorithm
+start = time.time()
+soln_scipy, _ = cgs(A_numpy, b_numpy)
+stop = time.time()
+print(f'Scipy time: {stop - start}s')
+
+print(soln_jax.mean())
+print(soln_scipy.mean())
 
 # Output the solution
-print("Conjugate Gradient Solution:", solution)
+# print("Conjugate Gradient Solution:", solution)
 # print("Info:", info)
