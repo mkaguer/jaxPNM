711a1acecb7d284339865b93fedca05fcb6516b6
Mike McKague <m.mckague19@gmail.com>
2024-11-19 13:57:47 -0500

    step 1 created jax network

--- scripts/optimization_flow.py
+++ scripts/optimization_flow.py
@@ -6,3 +6,27 @@ Created by: Mike McKague
 Date: November 18, 2024
 """
 
+import models as mods
+
+def make_cubic_network(shape, spacing=1, connectivity=6):
+    '''
+    Create custom cubic network that returns dictionary of JAX arrays.
+    '''
+    network = mods.cubic(shape=shape,
+                         spacing=spacing,
+                         connectivity=connectivity,
+                         node_prefix='pore',
+                         edge_prefix='throat')
+    return network
+
+
+if __name__ == "__main__":
+    
+    # Step 1: create JAX network
+    net = make_cubic_network(shape=[4, 1, 1], spacing=1e-4)
+    
+    # Step 2: add properties
+    # a) 
+    
+
+
