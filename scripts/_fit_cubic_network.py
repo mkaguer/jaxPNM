399da4ffd02a85e4349c16550121e716dbc6348a
Mike McKague <m.mckague19@gmail.com>
2025-01-28 14:34:53 -0500

    added fit_porosimetry function to FitCubicNetwork class

--- scripts/_fit_cubic_network.py
+++ scripts/_fit_cubic_network.py
@@ -4,6 +4,7 @@ import jax.experimental.sparse as js
 import matplotlib.pyplot as plt
 import diffrax as dfx
 import mypnmlib as pnm
+from jax import lax
 
 
 class FitCubicNetwork:
@@ -12,7 +13,7 @@ class FitCubicNetwork:
     differentiation in JAX
     """
 
-    def __init__(self, network, K_target):
+    def __init__(self, network, K_target=None, sat_target=None, pressure=None):
         r"""
         Initializes class with network attribute
 
@@ -22,10 +23,16 @@ class FitCubicNetwork:
             The network dictionary containing all important data
         K_target : float
             The target permeability that we are fitting our network to
+        sat_target : ndarray
+            The target saturation that we are fitting our network to
+        pressure : ndarray
+            The pressure corresponding to the target saturation data
 
         """
         self.network = network
         self.K_target = K_target
+        self.sat_target = sat_target
+        self.pressure = pressure
 
     def flow(self, D):
         r"""
@@ -68,13 +75,161 @@ class FitCubicNetwork:
 
         return x
 
-    def porosimetry():
-        ...
-        return
+    def find_invasion_pressure(self):
 
-    def fit_porosimetry():
-        ...
-        return
+        # get network
+        net = self.network
+        # get Nt and Np
+        Nt = len(net['throat.conns'])
+        Np = len(net['pore.volume'])
+        # get capillary pressure
+        pc = net['throat.entry_pressure']
+        # get maximum entry pressure
+        max_pc = jnp.max(pc)
+        # initialize invaded_throats and invaded_pores
+        invaded_throats = jnp.zeros(Nt, dtype=bool)
+        invaded_pores = net['pore.left']  # FIXME: update so it gets from bc
+
+        def body_fun(i, state):
+            # breakout state
+            invaded_throats, invaded_pores, pressure, invasion_pressure = state
+            # find throats connected to set of invaded pores
+            pores = jnp.where(invaded_pores, jnp.arange(Np), -1)
+            connected = pnm.models.find_neighbor_throats(net, pores)
+            # find invadable throats (set of connected but not invaded)
+            invadable = jnp.logical_xor(connected, invaded_throats)
+            # get index of invadable throat with minimum pressure
+            index = jnp.argmin(jnp.where(invadable, pc, max_pc*2))
+            # update pressure
+            pressure = jnp.maximum(pressure, pc[index])
+            # update invasion pressire for throat
+            invasion_pressure = invasion_pressure.at[index].set(pressure)
+            # add newly invaded throats
+            invaded_throats = invaded_throats.at[index].set(True)
+            # update invaded pores, find ALL pores neighboring invaded throats!
+            throats = jnp.where(invaded_throats, jnp.arange(Nt), -1)
+            invaded_pores = pnm.models.find_neighbor_pores(net, throats)
+            return invaded_throats, invaded_pores, pressure, invasion_pressure
+
+        # initialize state for the for loop
+        pressure = 0
+        invasion_pressure = jnp.zeros(Nt)
+        state0 = (invaded_throats, invaded_pores, pressure, invasion_pressure)
+        # run for loop
+        final_state = lax.fori_loop(0, Nt, body_fun, state0)
+        # get invasion pressure from state
+        invasion_pressure = final_state[-1]
+
+        return invasion_pressure
+
+    def run_invasion(self):
+
+        # get network
+        net = self.network
+        # get pressure 
+        pressure = self.pressure
+        # get invasion pressures (Nt,)
+        invasion_pressure = self.find_invasion_pressure()
+        # get volumes
+        vp = net['pore.volume']
+        vt = net['throat.volume']
+        total_volume = jnp.sum(vp) + jnp.sum(vt)
+
+        def body_func(i, state):
+            # breakout state
+            saturation = state
+            # apply sigmoid function
+            sf = 0.01
+            throat_prob = jax.nn.sigmoid((pressure[i] - invasion_pressure)/sf)
+            # find pore probability
+            pore_prob = pnm.models.get_max_of_neighbor_throats(net, throat_prob)
+            pore_prob = jnp.where(net['pore.left'], 1.0, pore_prob)
+            # calculate invaded volume
+            invaded_volume = jnp.sum(vp * pore_prob) + jnp.sum(vt * throat_prob)
+            # calculate saturation
+            sat = invaded_volume/total_volume
+            saturation = saturation.at[i].set(sat)
+            return saturation
+
+        # initialize saturation
+        # FIXME: minus bp vol
+        saturation = jnp.zeros(len(pressure), dtype=float)
+        # run for loop
+        state = saturation
+        sat = lax.fori_loop(0, len(pressure), body_func, state)
+
+        return sat
+
+    def R_squared(self, y, y_target):
+
+        SSE = jnp.sum((y - y_target)**2)
+        SST = jnp.sum((y_target - jnp.average(y_target))**2)
+        R2 = 1 - SSE/SST
+
+        return R2
+
+    def calc_sse(self, y, y_target):
+
+        SSE = jnp.sum((y - y_target)**2)
+
+        return SSE
+
+    def sat_loss(self, D):
+
+        # get network
+        net = self.network
+        # set pore diameter
+        net['pore.diameter'] = D
+        # regenerate geometry models
+        net['throat.diameter'] = pnm.models.throat_diameter(network=net)
+        net['throat.length'] = pnm.models.throat_length(network=net)
+        net['pore.volume'] = pnm.models.sphere(network=net)
+        net['throat.total_volume'] = pnm.models.cylinder(network=net)
+        net['throat.lens_volume'] = pnm.models.lens(network=net)
+        props = ['throat.total_volume', 'throat.lens_volume']
+        net['throat.volume'] = pnm.models.difference(network=net, props=props)
+        # add entry pressure model
+        net['throat.contact_angle'] = 120
+        net['throat.surface_tension'] = 0.072
+        Pc = pnm.models.washburn(network=net)
+        net['throat.entry_pressure'] = Pc
+        # run invasion simulation
+        sat = self.run_invasion()
+        # calculate SSE
+        SSE = self.calc_sse(sat, self.sat_target)
+        # calculate penalty
+        lbd = jnp.maximum(0.0 - D, 0)
+        ubd = jnp.maximum(D - 1.0, 0)
+        penalty = jnp.sum(lbd**2 + ubd**2)
+        # calculate loss
+        loss = SSE + penalty
+
+        return loss
+
+    def fit_porosimetry(self, D0, solver=dfx.Euler(), t_span=(0, 10), dt=1):
+
+        # retrieve loss function
+        f = self.sat_loss
+        # Define the gradient of f(x)
+        grad_f = jax.grad(f)
+
+        # Define the ODE system for gradient flow: dx/dt = -grad(f)
+        def dydt(t, y, args):
+            return jnp.clip(-grad_f(y), -10.0, 10.0)
+
+        # Time span (we treat the optimization as a "time" evolution)
+        t0, t1 = t_span
+        # Define the ODE problem
+        term = dfx.ODETerm(dydt)
+        # Solve the ODE, treating time as "iterations" for optimization
+        solution = dfx.diffeqsolve(term,
+                                   solver,
+                                   t0=t0, t1=t1, dt0=dt, y0=D0)
+        # The final x value after "evolving" it toward the minimum
+        D = solution.ys[-1]
+        loss = f(D)
+
+        return D, loss
 
     def calc_K(self, x):
 
@@ -112,6 +267,7 @@ class FitCubicNetwork:
         return mse
 
     def K_loss(self, D, net):
+        # FIXME: remove net from args?
         # run flow simulation
         x = self.flow(D)
         # calculate permeability
@@ -207,3 +363,96 @@ class FitCubicNetwork:
         plt.ylabel("Loss")
         plt.title(f"Loss Landscape Along D[{index}]")
         plt.show()
+
+
+if __name__ == "__main__":
+    
+    import os
+    from jax import config
+    import jax
+    import jax.numpy as jnp
+    from jax import lax
+    import mypnmlib as pnm
+    import matplotlib.pyplot as plt
+    import diffrax as dfx
+    import time
+
+    os.environ["JAX_PLATFORMS"] = "cpu"
+    config.update("jax_enable_x64", True)
+
+    # create network
+    spacing = 1
+    net = pnm.network.make_cubic_network(shape=[5, 5, 5], spacing=spacing)
+
+    # get Nt and Np
+    Nt = len(net['throat.conns'])
+    Np = len(net['pore.coords'])
+
+    # get diameters
+    key = jax.random.PRNGKey(0)
+    D = jax.random.uniform(key, shape=(Np,)) * spacing
+    # D = jnp.array([0.9, 0.5, 0.4, 0.25]) * spacing
+    print(f'Target diameter: {D}')
+    net['pore.diameter'] = D
+
+    # regenerate geometry models
+    net['throat.diameter'] = pnm.models.throat_diameter(net)
+    net['throat.length'] = pnm.models.throat_length(net)
+    net['pore.volume'] = pnm.models.sphere(net)
+    net['throat.total_volume'] = pnm.models.cylinder(net)
+    net['throat.lens_volume'] = pnm.models.lens(network=net)
+    props = ['throat.total_volume', 'throat.lens_volume']
+    net['throat.volume'] = pnm.models.difference(network=net, props=props)
+    # add entry pressure model
+    net['throat.contact_angle'] = 120
+    net['throat.surface_tension'] = 0.072
+    Pc = pnm.models.washburn(network=net)
+    net['throat.entry_pressure'] = Pc
+
+    # set the range of pressures to be investigated
+    pressures = jnp.arange(0.1, 2, 0.01)
+
+    # add the adjacency matrix
+    weights = jnp.arange(1, Nt+1)
+    am = pnm.network.create_adjacency_matrix(net, weights=weights, fmt='csr')
+    net['adjacency_matrix'] = am
+
+    # run invasion simulation
+    sat = run_invasion(net, pressures)
+
+    # plot
+    
+
+    # try to take gradient
+    grad_f = jax.grad(f)
+
+    # add sat_target and pressures to net
+    net['sat_target'] = sat
+    net['pressures'] = pressures
+    
+    # test grad_f working
+    # D = jnp.array([0.8, 0.55, 0.35, 0.2]) * spacing
+    
+    
+    start = time.time()
+    print(f'Initial gradient: {grad_f(y0, net)}')
+    stop = time.time()
+    print(f'Gradient time: {stop - start:.4f}s')
+    '''
+    # visualize loss as a function of one of the parameters!
+    D_vals = jnp.linspace(0.01, 1.0, 100)
+    losses = jnp.array([f(jnp.array([0.8, D, 0.35, 0.2]), net) for D in D_vals])
+
+    plt.plot(D_vals, losses)
+    plt.xlabel("D[0]")
+    plt.ylabel("Loss")
+    plt.title("Loss Landscape Along D[0]")
+    plt.show()
+    '''
+    
+    sat = run_invasion(net, pressures)
+    plt.figure(1)
+    plt.plot(pressures, sat, label='AI')
+    plt.legend()
+    plt.show()
+        
---
+++ scripts/example_fit_porosimetry.py
@@ -0,0 +1,79 @@
+import os
+from jax import config
+import jax
+import diffrax as dfx
+import jax.numpy as jnp
+import mypnmlib as pnm
+import matplotlib.pyplot as plt
+from _fit_cubic_network import FitCubicNetwork
+
+os.environ["JAX_PLATFORMS"] = "cpu"
+config.update("jax_enable_x64", True)
+
+# create network
+spacing = 1
+net = pnm.network.make_cubic_network(shape=[5, 5, 5], spacing=spacing)
+
+# get Nt and Np
+Nt = len(net['throat.conns'])
+Np = len(net['pore.coords'])
+
+# get target diameters
+key = jax.random.PRNGKey(0)
+D = jax.random.uniform(key, shape=(Np,)) * spacing
+net['pore.diameter'] = D
+
+# add geometry models
+net['throat.diameter'] = pnm.models.throat_diameter(net)
+net['throat.length'] = pnm.models.throat_length(net)
+net['pore.volume'] = pnm.models.sphere(net)
+net['throat.total_volume'] = pnm.models.cylinder(net)
+net['throat.lens_volume'] = pnm.models.lens(network=net)
+props = ['throat.total_volume', 'throat.lens_volume']
+net['throat.volume'] = pnm.models.difference(network=net, props=props)
+
+# add entry pressure model
+net['throat.contact_angle'] = 120
+net['throat.surface_tension'] = 0.072
+Pc = pnm.models.washburn(network=net)
+net['throat.entry_pressure'] = Pc
+
+# add the adjacency matrix
+weights = jnp.arange(1, Nt+1)
+am = pnm.network.create_adjacency_matrix(net, weights=weights, fmt='csr')
+net['adjacency_matrix'] = am
+
+# create instance of FitCubicNetwork
+pressure = jnp.arange(0.1, 2, 0.01)
+fcn = FitCubicNetwork(net, pressure=pressure)
+
+# add sat_target as attribute to fcn
+sat_target = fcn.run_invasion()
+fcn.sat_target = sat_target
+
+# plot target saturation
+plt.figure(1)
+plt.plot(pressure, sat_target, label='target')
+plt.xlabel('Pressures')
+plt.ylabel('Saturation')
+
+# get initial diameters
+key = jax.random.PRNGKey(1)
+D0 = jax.random.uniform(key, shape=(Np,)) * spacing
+print(f'Initial loss: {fcn.sat_loss(D0)}')
+
+# get initial saturation
+sat0 = fcn.run_invasion()
+plt.figure(1)
+plt.plot(pressure, sat0, label='Initial Guess')
+
+# fit porosimetry
+D, loss = fcn.fit_porosimetry(D0, solver=dfx.Euler(), t_span=(0, 1), dt=0.01)
+print(f'Final loss: {fcn.sat_loss(D)}')
+
+# plot AI porosimetry
+sat = fcn.run_invasion()
+plt.figure(1)
+plt.plot(pressure, sat, label='AI')
+plt.legend()
+plt.show()
