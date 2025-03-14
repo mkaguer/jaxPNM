196adddd5cfcf188ac6676c77f46003a78fc3f79
Mike McKague <m.mckague19@gmail.com>
2025-03-14 14:54:12 -0400

    Fixed sampling script to include tsd

--- scripts/sampling.py
+++ scripts/sampling.py
@@ -14,10 +14,26 @@ np.random.seed(1)
 # choose data
 image = "Berea"
 
+# load experimental data
+data = np.loadtxt('../data/porosimetry-' + image + '.csv', delimiter=',')
+mask = ~np.isinf(data[:, 0])
+sat_target = np.array(data[:, 1][mask])
+x_target = np.array(data[:, 0][mask])
+
+# transform experimental data via spacing!
+sigma = 0.4791  # N/m
+theta = 140  # radians!
+Dp = -4*sigma*np.cos(theta*np.pi/180)/x_target
+spacing = Dp[0]
+x_target = x_target * spacing
+
 # load fitted diameters
 net = np.load('../networks/fitted-' + image + '.npz')
 net = {key: net[key] for key in net.files}
-D = net['pore.diameter']
+Dp = net['pore.diameter']
+tsf = net['throat.tsf']
+conns = net['throat.conns']
+Dt = np.min(Dp[conns], axis=1) * tsf
 
 # create networks, one 10^3 and another N^3 for sampling test
 shape_f = [10, 10, 10]
@@ -47,39 +63,68 @@ def scale_coords(coords):
 coords_f = scale_coords(coords_f)
 coords_s = scale_coords(coords_s)
 
+# get throat coords, already scaled!
+conns_f = net_f['throat.conns'].copy()
+conns_s = net_s['throat.conns'].copy()
+throat_coords_f = np.sum(coords_f[conns_f], axis=1)/2
+throat_coords_s = np.sum(coords_s[conns_s], axis=1)/2
+
 # fit gaussian kde to D and take sample
-kde = gaussian_kde(D, bw_method=0.1)
-D_kde = kde.resample(Np)[0]
+kde = gaussian_kde(Dp, bw_method=0.1)
+Dp_kde = kde.resample(Np)[0]
 
 # fit GP to D and coords_f
 kernel = C(1.0) * RBF(length_scale=0.05)
-gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=5)
-gp.fit(coords_f, D)
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=0)
+gp.fit(coords_f, Dp)
+print('Finished GP Dp fit')
 
 # sample from GP
 tree = cKDTree(coords_f)
 _, indices = tree.query(coords_s)
 coords_s = coords_f[indices]
-D_gp, _ = gp.predict(coords_s, return_std=True)
+Dp_gp, _ = gp.predict(coords_s, return_std=True)
 
 # sort to preserve spatial trends
-indices = np.argsort(D_gp)
-values = np.sort(D_kde)
+indices = np.argsort(Dp_gp)
+values = np.sort(Dp_kde)
 # get sampled Ds
-D_sampled = np.zeros_like(D_gp)
-D_sampled[indices] = values
-D_sampled = np.clip(D_sampled, 1e-2, 1.0)
+Dp_sampled = np.zeros_like(Dp_gp)
+Dp_sampled[indices] = values
+Dp_sampled = np.clip(Dp_sampled, 1e-2, 1.0)
+
+# get sampled tsf
+kde = gaussian_kde(Dt, bw_method=0.1)
+Dt_kde = kde.resample(Nt)[0]
+
+# fit GP to tsf and throat_coords_f
+kernel = C(1.0) * RBF(length_scale=0.05)
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=0)
+gp.fit(throat_coords_f, Dt)
+print('Finished GP Dt fit')
+
+# sample from GP
+tree = cKDTree(throat_coords_f)
+_, indices = tree.query(throat_coords_s)
+throat_coords_s = throat_coords_f[indices]
+Dt_gp, _ = gp.predict(throat_coords_s, return_std=True)
+
+# sort to preserve spatial trends
+indices = np.argsort(Dt_gp)
+values = np.sort(Dt_kde)
+# get sampled Ds
+Dt_sampled = np.zeros_like(Dt_gp)
+Dt_sampled[indices] = values
+Dt_sampled = np.clip(Dt_sampled, 1e-2, 1.0)
+
+# ensure that Dt is not greater than Dp
+max_throat_size = np.min(Dp_sampled[conns_s], axis=1)
+mask = Dt_sampled >= max_throat_size
+Dt_sampled[mask] = max_throat_size[mask] * 0.99
 
 # add models to fitted network
-net_f['pore.diameter'] = D
-net_f.add_model(propname='throat.max_size',
-                model=op.models.misc._neighbor_lookups.from_neighbor_pores,
-                mode='min',
-                prop='pore.diameter')
-net_f.add_model(propname='throat.diameter',
-                model=op.models.misc._basic_math.scaled,
-                factor=0.5,
-                prop='throat.max_size')
+net_f['pore.diameter'] = Dp
+net_f['throat.diameter'] = Dt
 net_f.add_model(propname='throat.length',
                 model=op.models.geometry.throat_length._funcs.spheres_and_cylinders,
                 pore_diameter='pore.diameter',
@@ -100,15 +145,8 @@ net_f.add_model(propname='pore.volume',
                 pore_diameter='pore.diameter')
 
 # add models to sampled network
-net_s['pore.diameter'] = D_sampled
-net_s.add_model(propname='throat.max_size',
-                model=op.models.misc._neighbor_lookups.from_neighbor_pores,
-                mode='min',
-                prop='pore.diameter')
-net_s.add_model(propname='throat.diameter',
-                model=op.models.misc._basic_math.scaled,
-                factor=0.5,
-                prop='throat.max_size')
+net_s['pore.diameter'] = Dp_sampled
+net_s['throat.diameter'] = Dt_sampled
 net_s.add_model(propname='throat.length',
                 model=op.models.geometry.throat_length._funcs.spheres_and_cylinders,
                 pore_diameter='pore.diameter',
@@ -155,8 +193,9 @@ drn_s.set_inlet_BC(pores=inlet_s)
 drn_f.set_inlet_BC(pores=inlet_f)
 
 # run drainage
-drn_s.run(pressures=np.arange(0, 20, 0.1))
-drn_f.run(pressures=np.arange(0, 20, 0.1))
+pressures=np.arange(0, 20, 0.1)
+drn_s.run(pressures=pressures)
+drn_f.run(pressures=pressures)
 
 # get results
 pc_s, sat_s = drn_s.pc_curve()
@@ -164,25 +203,39 @@ pc_f, sat_f = drn_f.pc_curve()
 
 # plot results
 plt.figure(1)
-plt.plot(pc_s, sat_s, label=f'Fitted: {shape_f}')
-plt.plot(pc_f, sat_f, label=f'Sampled: {shape_s}')
+plt.plot(pc_f, sat_f, label=f'Fitted: {shape_f}')
+plt.plot(pc_s, sat_s, label=f'Sampled: {shape_s}')
+plt.xlabel('Pressure')
+plt.ylabel('Saturation')
+plt.legend()
+plt.show()
+
+# interpolate results
+sat_s = np.interp(x_target, pc_s, sat_s)
+sat_f = np.interp(x_target, pc_f, sat_f)
+
+# plot results
+plt.figure(2)
+plt.plot(x_target, sat_f, label=f'Fitted: {shape_f}')
+plt.plot(x_target, sat_s, label=f'Sampled: {shape_s}')
+plt.plot(x_target, sat_target, label='Experimental Data')
 plt.xlabel('Pressure')
 plt.ylabel('Saturation')
 plt.legend()
 plt.show()
 
 # plot psd distribution
-plt.figure(2)
-plt.hist(D, bins=30, alpha=0.5, density=True, label='Fitted')
-plt.hist(D_sampled, bins=30, alpha=0.5, density=True, label='Sampled')
+plt.figure(3)
+plt.hist(Dp, bins=30, alpha=0.5, density=True, label='Fitted')
+plt.hist(Dp_sampled, bins=30, alpha=0.5, density=True, label='Sampled')
 plt.legend()
 plt.show()
 
 # plot tsd distribution
 bins = np.arange(0, 0.5, 0.02)
 Dt_s = net_s['throat.diameter']
-Dt_f = net_f['throat.diameter'] 
-plt.figure(3)
+Dt_f = net_f['throat.diameter']
+plt.figure(4)
 plt.hist(Dt_f, bins=bins, alpha=0.5, density=True, label='Fitted')
 plt.hist(Dt_s, bins=bins, alpha=0.5, density=True, label='Sampled')
 plt.legend()
