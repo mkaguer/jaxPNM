06b322e7ec86d39ca4a60fd7150dd20cef1c1587
Mike McKague <m.mckague19@gmail.com>
2025-03-28 18:26:04 -0400

    Wrote script for plotting sampled that also works with flow!

--- scripts/plot_sampled.py
+++ scripts/plot_sampled.py
@@ -12,13 +12,15 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from scipy.spatial import cKDTree
 import jax
+import pandas as pd
+import porespy as ps
 
 np.random.seed(0)
 
 os.environ["JAX_PLATFORMS"] = "cpu"
 config.update("jax_enable_x64", False)
 
-image = "Berea"
+image = "S5"
 bw_method = 0.01
 
 # set properties
@@ -26,7 +28,7 @@ sigma = 0.4791
 theta = 140
 
 # create sampled JAX network
-shape = [20, 15, 15]
+shape = [15, 15, 15] # [20, 15, 15]
 net_s = pnm.network.make_cubic_network(shape=shape, spacing=1)
 
 # load fitted network
@@ -69,6 +71,67 @@ x_target = jnp.array(data[:, 0][mask]).astype(jnp.float32)
 # calculate spacing
 Dp = -4*sigma*jnp.cos(theta*jnp.pi/180)/x_target
 spacing = Dp[0]
+
+# add "constant" Gh properties to sampled network
+net_s['pore.viscosity'] = jnp.ones(Np_s) * 1e-3
+net_s['throat.viscosity'] = jnp.ones(Nt_s) * 1e-3
+
+# set BCs in x direction
+pores = jnp.where(net_s['pore.left'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=1.0,
+                       mode='overwrite')
+pores = jnp.where(net_s['pore.right'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=0.0,
+                       mode='add')
+net_s['pore.bc.valuex'] = net_s['pore.bc.value']
+net_s['pore.bc.maskx'] = net_s['pore.bc.mask']
+net_s['boundary_poresx'] = net_s['boundary_pores']
+net_s['rate_poresx'] = pores
+del net_s['pore.bc.value'], net_s['pore.bc.mask'], net_s['boundary_pores']
+
+# set BCs in y direction
+pores = jnp.where(net_s['pore.front'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=1.0,
+                       mode='overwrite')
+pores = jnp.where(net_s['pore.back'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=0.0,
+                       mode='add')
+net_s['pore.bc.valuey'] = net_s['pore.bc.value']
+net_s['pore.bc.masky'] = net_s['pore.bc.mask']
+net_s['boundary_poresy'] = net_s['boundary_pores']
+net_s['rate_poresy'] = pores
+del net_s['pore.bc.value'], net_s['pore.bc.mask'], net_s['boundary_pores']
+
+# set BCs in z direction
+pores = jnp.where(net_s['pore.bottom'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=1.0,
+                       mode='overwrite')
+pores = jnp.where(net_s['pore.top'])[0]
+pnm.simulations.set_BC(net_s,
+                       pores=pores,
+                       bctype='value',
+                       bcvalues=0.0,
+                       mode='add')
+net_s['pore.bc.valuez'] = net_s['pore.bc.value']
+net_s['pore.bc.maskz'] = net_s['pore.bc.mask']
+net_s['boundary_poresz'] = net_s['boundary_pores']
+net_s['rate_poresz'] = pores
+del net_s['pore.bc.value'], net_s['pore.bc.mask'], net_s['boundary_pores']
 
 # create fcn objects
 pressures = jnp.arange(jnp.min(x_target)*0.9, jnp.max(x_target)*1.1, 1e3)
@@ -126,7 +189,7 @@ throat_coords_s = jnp.sum(coords_s[conns_s], axis=1)/2
 kde = gaussian_kde(Dp_fitted, bw_method=bw_method)
 Dp_kde = kde.resample(Np_s)[0]  # this is a numpy array!
 
-# fit GP to D and coords_f
+# fit GP to Dp and coords_f
 kernel = C(1.0) * RBF(length_scale=0.05)
 gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=0)
 gp.fit(coords_f, Dp_fitted)
@@ -148,45 +211,35 @@ Dp_sampled = np.clip(Dp_sampled, 1e-2, 0.99)
 Dp_sampled = jnp.array(Dp_sampled)  # convert back to jax array
 
 # fit gaussian kde to Dt and sample
-kde = gaussian_kde(Dt_fitted, bw_method=bw_method)
-Dt_kde = kde.resample(Nt_s)[0]  # this is a numpy array
+kde = gaussian_kde(tsf_fitted, bw_method=bw_method)
+tsf_kde = kde.resample(Nt_s)[0]  # this is a numpy array
 
-# fit GP to Dt and throat_coords_f
+# fit GP to tsf and sample
 kernel = C(1.0) * RBF(length_scale=0.05)
 gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=0)
-gp.fit(throat_coords_f, Dt_fitted)
+X_train = np.hstack([coords_f[conns_f[:, 0]], coords_f[conns_f[:, 1]]]) 
+gp.fit(X_train, tsf_fitted)
 print('Finished GP Dt fit')
 
-# sample from GP
-tree = cKDTree(throat_coords_f)
-_, indices = tree.query(throat_coords_s)
-throat_coords_s = throat_coords_f[indices]
-Dt_gp, _ = gp.predict(throat_coords_s, return_std=True) # this is a numpy array
+X_pred = np.hstack([coords_s[conns_s[:, 0]], coords_s[conns_s[:, 1]]]) 
+tsf_gp, _ = gp.predict(X_pred, return_std=True)
 
 # sort to preserve spatial trends
-indices = np.argsort(Dt_gp)
-values = np.sort(Dt_kde)
+indices = np.argsort(tsf_gp)
+values = np.sort(tsf_kde)
 # get sampled Ds
-Dt_sampled = np.zeros_like(Dt_gp)
-Dt_sampled[indices] = values
-Dt_sampled = np.clip(Dt_sampled, 1e-2, 0.99)
-Dt_sampled = jnp.array(Dt_sampled)  # convert back to jax array
+tsf_sampled = np.zeros_like(tsf_gp)
+tsf_sampled[indices] = values
+tsf_sampled = np.clip(tsf_sampled, 1e-2, 0.95)  # FIXME: should this be 0.95?
+tsf_sampled = jnp.array(tsf_sampled)  # convert back to jax array
 
-# ensure that Dt is not greater than Dp
-max_throat_size = jnp.min(Dp_sampled[conns_s], axis=1)
-mask = Dt_sampled >= max_throat_size
-Dt_sampled = jnp.where(mask, max_throat_size * 0.99, Dt_sampled)
-
-# convert Dt_sampled to tsf_sampled
-tsf_sampled = Dt_sampled/max_throat_size
+# sample from GP
+Dt_sampled = tsf_sampled * jnp.min(Dp_sampled[conns_s], axis=1)
 
 # get sampled diameters without GP
 Dp_sampled_n = jnp.clip(Dp_kde, 1e-2, 0.99)
-Dt_sampled_n = jnp.clip(Dt_kde, 1e-2, 0.99)
-max_throat_size = jnp.min(Dp_sampled_n[conns_s], axis=1)
-mask = Dt_sampled_n >= max_throat_size
-Dt_sampled_n = jnp.where(mask, max_throat_size * 0.99, Dt_sampled_n)
-tsf_sampled_n = Dt_sampled_n/max_throat_size
+tsf_sampled_n = jnp.clip(tsf_kde, 1e-2, 0.95)
+Dt_sampled_n = tsf_sampled_n * jnp.min(Dp_sampled_n[conns_s], axis=1)
 
 # create copy of network BEFORE jitting
 net_copy = fcn_s.network.copy() 
@@ -197,6 +250,49 @@ jitted_run_invasion_f = jax.jit(fcn_f.run_invasion)
 sat_s = jitted_run_invasion_s(Dp_sampled, tsf_sampled)
 sat_n = jitted_run_invasion_s(Dp_sampled_n, tsf_sampled_n)
 sat_f = jitted_run_invasion_f(Dp_fitted, tsf_fitted)
+
+# calculate permeabilities for fitted
+px = fcn_f.flow(Dp_fitted, tsf_fitted, axis='x')
+Kx = fcn_f.calc_K(px, axis='x')
+py = fcn_f.flow(Dp_fitted, tsf_fitted, axis='y')
+Ky = fcn_f.calc_K(py, axis='y')
+pz = fcn_f.flow(Dp_fitted, tsf_fitted, axis='z')
+Kz = fcn_f.calc_K(pz, axis='z')
+K_avg = jnp.average(jnp.array([Kx, Ky, Kz]))
+K_fitted = jnp.array([Kx, Ky, Kz, K_avg])*spacing**2/ 0.98e-12 * 1000
+
+# calculate permeabilities for GP
+px = fcn_s.flow(Dp_sampled, tsf_sampled, axis='x')
+Kx = fcn_s.calc_K(px, axis='x')
+py = fcn_s.flow(Dp_sampled, tsf_sampled, axis='y')
+Ky = fcn_s.calc_K(py, axis='y')
+pz = fcn_s.flow(Dp_sampled, tsf_sampled, axis='z')
+Kz = fcn_s.calc_K(pz, axis='z')
+K_avg = jnp.average(jnp.array([Kx, Ky, Kz]))
+K_sampled = jnp.array([Kx, Ky, Kz, K_avg])*spacing**2/ 0.98e-12 * 1000
+
+# calculate permeabilities for no GP
+px = fcn_s.flow(Dp_sampled_n, tsf_sampled_n, axis='x')
+Kx = fcn_s.calc_K(px, axis='x')
+py = fcn_s.flow(Dp_sampled_n, tsf_sampled_n, axis='y')
+Ky = fcn_s.calc_K(py, axis='y')
+pz = fcn_s.flow(Dp_sampled_n, tsf_sampled_n, axis='z')
+Kz = fcn_s.calc_K(pz, axis='z')
+K_avg = jnp.average(jnp.array([Kx, Ky, Kz]))
+K_sampled_n = jnp.array([Kx, Ky, Kz, K_avg])*spacing**2/ 0.98e-12 * 1000
+
+# load permeability data
+data = pd.read_csv('../data/K-' + image + '.csv', header=None).values.flatten()
+K_target = jnp.array(data).astype(jnp.float32)
+K_target = jnp.concatenate((K_target, jnp.array([jnp.average(K_target)])))
+
+# Print permeability
+print(f'Target permeability: {K_target} mD')
+print(f'Final permeability: {K_sampled} mD')
+
+plt.hist(jnp.log10(net_s['throat.conductance']), bins=50, density=True, alpha=0.5)
+plt.hist(jnp.log10(net_f['throat.conductance']), bins=50, density=True, alpha=0.5)
+plt.show()
 
 # interpolate
 sat_s = jnp.interp(x_target, pressures, sat_s)
@@ -219,7 +315,7 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.legend(fontsize=12)
-plt.savefig('../figures/sampled-psd-' + image + f'-{bw_method}' + '.png')
+plt.savefig('../figures/sampled-psd-' + image + f'-{shape}' + '.png')
 plt.show()
 
 # plot tsd distribution
@@ -239,7 +335,7 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.legend(fontsize=12)
-plt.savefig('../figures/sampled-tsd-' + image + f'-{bw_method}' + '.png')
+plt.savefig('../figures/sampled-tsd-' + image + f'-{shape}' + '.png')
 plt.show()
 
 # plot pc results
@@ -264,9 +360,32 @@ plt.yticks(fontsize=12)
 plt.title(image, fontsize=16, fontweight='semibold')
 plt.grid(axis='x', linestyle='--', alpha=0.7)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.legend(loc='best', fontsize=12)
+plt.legend(loc='best', fontsize=12, frameon=True)
 plt.tight_layout()
-plt.savefig('../figures/sampled-porosimetrty-' + image + f'-{bw_method}' + '.png')
+plt.savefig('../figures/sampled-porosimetrty-' + image + f'-{shape}' + '.png')
+plt.show()
+
+# plot K results
+plt.figure(4, dpi=500)
+ax = plt.gca()  # Get current axes
+# Make the bounding box bold
+for spine in ax.spines.values():
+    spine.set_linewidth(2)
+ax.tick_params(direction='in', length=5, width=2) 
+x = jnp.arange(len(K_fitted))
+bar_width = 0.2
+plt.bar(x, K_target, width=bar_width, label='Target', color='tab:green')
+plt.bar(x + bar_width, K_fitted, width=bar_width, label='JAX', color='tab:purple')
+plt.bar(x + 2 * bar_width, K_sampled, width=bar_width, label='GP', color='tab:orange')
+plt.bar(x + 3 * bar_width, K_sampled_n, width=bar_width, label='No GP', color='tab:pink')
+plt.ylabel('Permeability (mD)', fontsize=14, fontweight='normal')
+plt.title(image, fontsize=16, fontweight='semibold')
+plt.xticks(x + bar_width*3/2, ['X', 'Y', 'Z', 'Avg'], fontsize=12, fontweight='normal')
+plt.yticks(fontsize=12, fontweight='normal')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.07), ncol=4, fontsize=12, frameon=True)
+plt.tight_layout()
+plt.savefig('../figures/sampled-permeability-' + image + f'-{shape}' + '.png')
 plt.show()
 
 # save sampled network
@@ -282,4 +401,4 @@ for key in net_s.keys():
 dic['pore.diameter'] = Dp_sampled
 dic['throat.tsf'] = tsf_sampled
 
-np.savez_compressed("../networks/sampled-" + image + ".npz", **dic)
+np.savez_compressed("../networks/sampled-" + image + f'-{shape}' + ".npz", **dic)
