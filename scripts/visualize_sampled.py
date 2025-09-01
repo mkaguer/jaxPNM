c8bf924976956c189b877fb6e80c0f2c162e4b07
Mike McKague <m.mckague19@gmail.com>
2025-09-01 09:40:25 -0400

    make visualized_sampled.py work for equivalent

--- scripts/visualize_sampled.py
+++ scripts/visualize_sampled.py
@@ -1,14 +1,19 @@
 import numpy as np
 import openpnm as op
 
-image = "Berea"
+image = "A1"
 BoT = "BoT-"
+equivalent = True
 
 shape = [15, 15, 15]
 
 # load sampled network
-data = np.load('../networks/sampled-' + BoT + image + f'-{shape}' + '.npz')
-data = {key: np.array(data[key]) for key in data.files}
+if equivalent is True:
+    data = np.load('../networks/sampled-eq-' + BoT + image + f'-{shape}' + '.npz')
+    data = {key: np.array(data[key]) for key in data.files}
+else:
+    data = np.load('../networks/sampled-' + BoT + image + f'-{shape}' + '.npz')
+    data = {key: np.array(data[key]) for key in data.files}
 
 # create a new clearn network from data
 net_s = {}
@@ -29,8 +34,12 @@ tsf = data['throat.tsf']
 net_s['throat.diameter'] = tsf * np.min(D[conns], axis=1)
 
 # load fitted network
-data = np.load('../networks/fitted-' + BoT + image + '.npz')
-data = {key: np.array(data[key]) for key in data.files}
+if equivalent is True:
+    data = np.load('../networks/fitted-eq-' + BoT + image + '.npz')
+    data = {key: np.array(data[key]) for key in data.files}
+else:
+    data = np.load('../networks/fitted-' + BoT + image + '.npz')
+    data = {key: np.array(data[key]) for key in data.files}
 
 # create a new clearn network from data
 net_f = {}
@@ -129,10 +138,16 @@ drn_f.run(pressures=pressures)
 pn_s['throat.radius'] = pn_s['throat.diameter']/2
 pn_s['pore.invasion_sequence'] = drn_s['pore.invasion_sequence']
 pn_s['throat.invasion_sequence'] = drn_s['throat.invasion_sequence']
-op.io.project_to_vtk(project=pn_s.project, filename='../paraview/network-' + BoT + image + '-sampled')
+if equivalent is True:
+    op.io.project_to_vtk(project=pn_s.project, filename='../paraview/network-eq-' + BoT + image + '-sampled')
+else:
+    op.io.project_to_vtk(project=pn_s.project, filename='../paraview/network-' + BoT + image + '-sampled')
 
 # save fitted network
 pn_f['throat.radius'] = pn_f['throat.diameter']/2
 pn_f['pore.invasion_sequence'] = drn_f['pore.invasion_sequence']
 pn_f['throat.invasion_sequence'] = drn_f['throat.invasion_sequence']
-op.io.project_to_vtk(project=pn_f.project, filename='../paraview/network-' + BoT + image + '-fitted')
+if equivalent is True:
+    op.io.project_to_vtk(project=pn_f.project, filename='../paraview/network-eq-' + BoT + image + '-fitted')
+else:
+    op.io.project_to_vtk(project=pn_f.project, filename='../paraview/network-' + BoT + image + '-fitted')
